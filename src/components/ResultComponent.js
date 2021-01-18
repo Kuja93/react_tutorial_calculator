d28d6b0feffe1a03e906f3d2ada1ed89e70f6d2d
Jonny <jonathanadonnell@msn.com>
2021-01-18 18:06:58 +0000

    test push

---
+++ src/components/ResultComponent.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class ResultComponent extends Component {
+    render() {
+        const {result} = this.props;
+        return (
+            <div className="result">
+                <p>{result}</p>
+            </div>
+        );
+    }
+}
+
+export default ResultComponent;
