6ca49024ed7599165eabe86256744d971a8d42d5
Jonny <jonathanadonnell@msn.com>
2021-01-28 20:27:02 +0000

    added responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
+import './Responsive.css';
+
 import ResultComponent from './components/ResultComponent';
 import KeyPadComponent from "./components/KeyPadComponent";
 
